c757bd2c67c9267ad3681ff48ad474a9a232c260
Ronald Manzano <rslmanzano@gmail.com>
2019-04-14 19:51:36 +1000

    https://www.youtube.com/watch?v=dVvgLUHdS_E

--- firebaseFn/dbschema.js
+++ firebaseFn/dbschema.js
@@ -4,6 +4,8 @@ let db = {
             userHandle: "user",
             body: "this is a scream body",
             createdAt: "2019-04-14T08:24:29.811Z",
+            likeCount: 5,
+            commentCount: 2
         },
     ],
 }
--- firebaseFn/functions/index.js
+++ firebaseFn/functions/index.js
@@ -6,10 +6,22 @@ admin.initializeApp()
 const express = require("express")
 const app = express()
 
+const config = {
+    apiKey: "AIzaSyAR7Z3hWBo9lxukHeTGDNO8O_Is9cIK_y0",
+    authDomain: "socialape-c4bb8.firebaseapp.com",
+    databaseURL: "https://socialape-c4bb8.firebaseio.com",
+    projectId: "socialape-c4bb8",
+    storageBucket: "socialape-c4bb8.appspot.com",
+    messagingSenderId: "269341467780",
+}
+
+const firebase = require("firebase")
+firebase.initializeApp(config)
+
+const db = admin.firestore()
+
 app.get("/screams", (req, res) => {
-    admin
-        .firestore()
-        .collection("screams")
+    db.collection("screams")
         .orderBy("createdAt", "desc")
         .get()
         .then(data => {
@@ -35,12 +47,10 @@ app.post("/scream", (req, res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
     }
 
-    admin
-        .firestore()
-        .collection("screams")
+    db.collection("screams")
         .add(newScream)
         .then(doc => {
             res.json({ message: `Document ${doc.id} created successfuly.` })
@@ -51,4 +61,53 @@ app.post("/scream", (req, res) => {
         })
 })
 
-exports.api = functions.region('asia-northeast1').https.onRequest(app)
+// Signup route
+
+app.post("/signup", (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    }
+
+    // TODO: validate data
+    let token, userId
+    db.doc(`/users/${newUser.handle}`)
+        .get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({ handle: "this handle is already taken." })
+            } else {
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => {
+            userId = data.user.uid
+            return data.user.getIdToken()
+        })
+        .then(idToken => {
+            token = idToken
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId,
+            }
+
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+        })
+        .then(() => {
+            return res.status(201).json({ token })
+        })
+        .catch(err => {
+            console.error(err)
+            if (err.code === "auth/email-already-in-use") {
+                return res.status(400).json({ email: "Email is already in use" })
+            } else {
+                return res.status(500).json({ error: err.code })
+            }
+        })
+})
+
+exports.api = functions.region("asia-northeast1").https.onRequest(app)
