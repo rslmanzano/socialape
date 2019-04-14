8096fd87df4c83e34b02b0c624c9e8acf3cf4350
Ronald Manzano <rslmanzano@gmail.com>
2019-04-15 08:21:09 +1000

    Full Stack React & Firebase: #5 Validation & Login Route https://www.youtube.com/watch?v=uK9EneE8G1M

--- firebaseFn/functions/index.js
+++ firebaseFn/functions/index.js
@@ -61,8 +61,18 @@ app.post("/scream", (req, res) => {
         })
 })
 
-// Signup route
+const isEmpty = string => {
+    if (string.trim() === "") return true
+    else return false
+}
 
+const isEmail = email => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (email.match(emailRegEx)) return true
+    else return false
+}
+
+// Signup route
 app.post("/signup", (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -72,6 +82,20 @@ app.post("/signup", (req, res) => {
     }
 
     // TODO: validate data
+    let errors = {}
+
+    if (isEmpty(newUser.email)) {
+        errors.email = "Must not be empty"
+    } else if (!isEmail(newUser.email)) {
+        errors.email = "Must be a valid email"
+    }
+
+    if (isEmpty(newUser.password)) errors.password = "Must not be empty"
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = "Passwords must match"
+    if (isEmpty(newUser.handle)) errors.handle = "Must not be empty"
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+    //
     let token, userId
     db.doc(`/users/${newUser.handle}`)
         .get()
@@ -107,6 +131,36 @@ app.post("/signup", (req, res) => {
             } else {
                 return res.status(500).json({ error: err.code })
             }
+        })
+})
+
+app.post("/login", (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    }
+    let errors = {}
+
+    if (isEmpty(user.email)) errors.email = "Must not be empty"
+    if (isEmpty(user.password)) errors.password = "Must not be empty"
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken()
+        })
+        .then(token => {
+            return res.json({ token })
+        })
+        .catch(err => {
+            console.error(err)
+            if (err.code === "auth/wrong-password") {
+                return res.status(403).json({ error: "Wrong credentials, please try again" })
+            }
+            return res.status(400).json({ error: err.code })
         })
 })
 
