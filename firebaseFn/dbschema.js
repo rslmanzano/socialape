3ad521c043420930840065cade46d8b38ca3c056
Ronald Manzano <rslmanzano@gmail.com>
2019-04-14 18:53:30 +1000

    Finished Video 3

---
+++ firebaseFn/dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+    screams: [
+        {
+            userHandle: "user",
+            body: "this is a scream body",
+            createdAt: "2019-04-14T08:24:29.811Z",
+        },
+    ],
+}
--- firebaseFn/functions/index.js
+++ firebaseFn/functions/index.js
@@ -3,37 +3,39 @@ const admin = require("firebase-admin")
 
 admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require("express")
+const app = express()
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
-    res.send("Hello from Firebase!")
-})
-
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get("/screams", (req, res) => {
     admin
         .firestore()
         .collection("screams")
+        .orderBy("createdAt", "desc")
         .get()
         .then(data => {
             let screams = []
             data.forEach(doc => {
-                screams.push(doc.data())
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt,
+                })
             })
             return res.json(screams)
         })
         .catch(err => console.error(err))
 })
 
-exports.createScream = functions.https.onRequest((req, res) => {
+app.post("/scream", (req, res) => {
     if (req.method !== "POST") {
-        return res.status(400).json({ error: "Method not allowed"})
+        return res.status(400).json({ error: "Method not allowed" })
     }
 
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+        createdAt: new Date().toISOString()
     }
 
     admin
@@ -48,3 +50,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
             console.error(err)
         })
 })
+
+exports.api = functions.region('asia-northeast1').https.onRequest(app)
